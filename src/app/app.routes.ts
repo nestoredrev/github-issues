8554d59a76fea1fc08aff8366320006687ef3033
Nestor <nestor.edrev@gmail.com>
2025-02-04 14:47:10 +0100

    First Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'issues', 
+        loadComponent: () => import('./modules/issues/pages/isssues-list/isssues-list-page.component')
+    },
+    {   path: 'issue/:number', 
+       loadComponent: () => import('./modules/issues/pages/issue/issue-page.component')
+    },
+    {   path: '**', 
+        redirectTo: 'issues'
+    },
+];
---
+++ src/app/modules/issues/pages/isssues-list/isssues-list-page.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-isssues-list-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './isssues-list-page.component.html'
+})
+export default class IsssuesListPageComponent { }
